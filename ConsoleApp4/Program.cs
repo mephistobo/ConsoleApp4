1717ef673401e339914374de9f128c56faf37adc
Bir Bogdan <studentksm14@gmail.com>
2020-04-24 05:45:00 +0300

    5

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -9,6 +9,7 @@ namespace ConsoleApp4
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!!!!!!!!!!!!!!");
         }
     }
 }
