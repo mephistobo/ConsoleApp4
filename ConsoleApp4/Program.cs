ea11c0bc9681af4ba9f640b064c1e2b31ac60247
Bir Bogdan <studentksm14@gmail.com>
2020-04-24 05:37:29 +0300

    3

--- ConsoleApp4/Program.cs
+++ ConsoleApp4/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp4
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!");
         }
     }
 }
